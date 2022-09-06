e74e1fba8caedbd80cd80a66686fc61a8f8e16d7
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-09-06 15:58:30 -0400

    resize after element is moved

--- main.js
+++ main.js
@@ -194,14 +194,14 @@ function dump() {
 dumpBtn.addEventListener('click', dump);
 
 window.addEventListener('hashchange', () => {
-    var rect = $('#textbox').parentElement.getBoundingClientRect();
-    $('#textbox').setAttribute('style', `width:${rect.width}px;height:${rect.height}px`);
-    textbox.resize(true);
     var where = 'main>div.flex-row.padding'
     if (location.hash === '#dump') {
         dump();
         where = '#dump>div';
     }
     $(where).insertBefore(statusBar, $(where).firstChild);
+    var rect = $('#textbox').parentElement.getBoundingClientRect();
+    $('#textbox').setAttribute('style', `width:${rect.width}px;height:${rect.height}px`);
+    textbox.resize(true);
 });
 if (location.hash === '#dump') window.dispatchEvent(new Event('hashchange'));
