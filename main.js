9cee0f880112173a80dd7d08713697c1cde6041e
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-09-08 13:45:27 -0400

    change default

--- main.js
+++ main.js
@@ -179,7 +179,7 @@ try {
         showStatus('Loaded from localStorage.', 'green');
     } else {
         textbox.setValue(`
-<langton><breed species="Beetle" name="langton"><case cell="0"><action><command name="put">1</command><command name="rt"></command><command name="fd"></command><command name="play">C2</command></action></case><case cell="1"><action><command name="put">0</command><command name="lt"></command><command name="fd"></command><command name="play">G2</command></action></case></breed><ant breed="langton" x="0" y="0" dir="1"></ant></langton>
+<langton><breed species="Beetle" name="langton"><case cell="0"><action><command name="put">1</command><command name="rt"></command><command name="fd"></command><command name="play">C2:##dir'2*3/1-;</command></action></case><case cell="1"><action><command name="put">0</command><command name="lt"></command><command name="fd"></command><command name="play">G2:##dir'2*3/1-;</command></action></case></breed><ant breed="langton" x="0" y="0" dir="1"></ant></langton>
 `);
         load();
     }
