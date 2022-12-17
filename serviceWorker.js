0d30613746c1fd80552a20d38f9ed02adbaff287
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-12-17 15:52:48 -0500

    typo

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = "langton-music-v2";
 
 // Copied from https://developer.chrome.com/docs/workbox/caching-strategies-overview/#stale-while-revalidate
 self.addEventListener('fetch', e => {
-    event.respondWith(caches.open(CACHE_NAME).then(cache => {
+    e.respondWith(caches.open(CACHE_NAME).then(cache => {
         return cache.match(e.request).then(cachedResponse => {
             const fetchedResponse = fetch(e.request).then(networkResponse => {
                 cache.put(e.request, networkResponse.clone());
