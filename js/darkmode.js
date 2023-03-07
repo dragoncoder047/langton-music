abe10f6a8f9afd438551ef3d3a1a8f096bd930a1
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-03-07 14:09:50 -0500

    revise dark mode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,7 +1,16 @@
 var DARK_MODE = false;
-(function() {
-    if (!window.matchMedia) return;
-    var mm = window.matchMedia("(prefers-color-scheme: dark)");
-    darkMode = mm.matches;
-    mm.addEventListener('change', () => darkMode = mm.matches);
+(function () {
+    if (window.matchMedia) {
+        var mm = window.matchMedia("(prefers-color-scheme: dark)");
+        darkMode = mm.matches;
+        mm.addEventListener('change', () => {
+            darkMode = mm.matches;
+        });
+    } else {
+        var h1 = document.querySelector('h1');
+        setInterval(() => {
+            var s = window.getComputedStyle(h1);
+            DARK_MODE = s.getPropertyValue('color') == 'white';
+        }, 200);
+    }
 })();
