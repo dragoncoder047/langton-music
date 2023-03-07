f66d5843eca83cc60ce10f6251992e9567184a97
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-03-07 14:15:33 -0500

    fix dark mode name issue

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,9 +2,9 @@ var DARK_MODE = false;
 (function () {
     if (window.matchMedia) {
         var mm = window.matchMedia("(prefers-color-scheme: dark)");
-        darkMode = mm.matches;
+        DARK_MODE = mm.matches;
         mm.addEventListener('change', () => {
-            darkMode = mm.matches;
+            DARK_MODE = mm.matches;
         });
     } else {
         var h1 = document.querySelector('h1');
