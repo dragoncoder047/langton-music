ea585d386fb3a1f0ae3077a547ef00b88ea33da1
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-10-11 17:43:37 +0000

    get interpolator working

--- js/interpol.js
+++ js/interpol.js
@@ -1,4 +1,4 @@
-function* level1ParseExpression(string, delimiters, singletons = [], openers = "[{(\"'", closers = "]})\"'", splitAtParens = true) {
+function* level1ParseExpression(string, delimiters, singletons = [], openers = "[{(", closers = "]})", stringers = "\"'", requireOpener = "") {
     var origString = string;
     var currentString = "";
     var stack = [];
@@ -9,7 +9,7 @@ function* level1ParseExpression(string, delimiters, singletons = [], openers = "
     mainloop:
     while (string) {
         for (var o of openers) {
-            if (string.startsWith(o)) {
+            if (string.startsWith(o) && (!requireOpener || stack.some(x => x[0] == requireOpener) || o == requireOpener)) {
                 if (stack.length > 0 && otc[o] === o && otc[o] === stack[stack.length - 1][0]) {
                     stack.pop();
                     if (stack.length == 0) {
@@ -28,18 +28,46 @@ function* level1ParseExpression(string, delimiters, singletons = [], openers = "
                 continue mainloop;
             }
         }
-        for (var c of closers) {
-            if (string.startsWith(c)) {
-                if (stack.length === 0) throw `unopened ${c}\n${origString}\n${" ".repeat(i)}^`;
-                var b = otc[stack.pop()[0]];
-                if (b !== c) throw `paren mismatch ${b} ${c}\n${origString}\n${" ".repeat(i)}^`;
-                if (stack.length == 0 && currentString.length > 0) {
-                    yield currentString + c;
-                    currentString = "";
-                } else currentString += c;
-                string = string.slice(c.length);
-                i += c.length;
+        for (var s of stringers) {
+            if (string.startsWith(s) && (!requireOpener || stack.some(x => x[0] == requireOpener) || s == requireOpener)) {
+                if (stack.length > 0 && stack[stack.length - 1][0] == s) {
+                    stack.pop();
+                    if (stack.length == 0) {
+                        yield currentString + s;
+                        currentString = "";
+                    } else currentString += s;
+                } else {
+                    if (stack.length == 0 && currentString.length > 0) {
+                        yield currentString;
+                        currentString = s;
+                    } else currentString += s;
+                    stack.push([s, i]);
+                }
+                string = string.slice(s.length);
+                i += s.length;
                 continue mainloop;
+            }
+        }
+        if (stack.length > 0 && stringers.indexOf(stack[stack.length - 1][0]) != -1) {
+            currentString += string[0];
+            i++;
+            string = string.slice(1);
+            continue mainloop;
+        }
+        if (!requireOpener || stack.some(x => x[0] == requireOpener)) {
+            for (var c of closers) {
+                if (string.startsWith(c)) {
+                    if (stack.length === 0) throw `unopened ${c}\n${origString}\n${" ".repeat(i)}^`;
+                    var b = otc[stack.pop()[0]];
+                    if (b !== c) throw `paren mismatch: expected '${b}', got '${c}'\n${origString}\n${" ".repeat(i)}^`;
+                    if (stack.length == 0 && currentString.length > 0) {
+                        yield currentString + c;
+                        currentString = "";
+                    } else currentString += c;
+                    string = string.slice(c.length);
+                    i += c.length;
+                    continue mainloop;
+                }
             }
         }
         if (stack.length == 0) {
@@ -67,12 +95,16 @@ function* level1ParseExpression(string, delimiters, singletons = [], openers = "
         string = string.slice(1);
         i++;
     }
-    if (stack.length > 0) throw `unclosed ${stack[0][0]}\n${origString}\n${" ".repeat(stack[0][1])}^`
+    if (stack.length > 0) {
+        var [lastC, lastI] = stack[stack.length - 1];
+        if (stringers.indexOf(lastC) == -1) throw `unclosed ${lastC}\n${origString}\n${" ".repeat(lastI)}^`;
+        throw `unclosed string\n${origString}\n${" ".repeat(lastI)}^`;
+    }
     yield currentString;
 }
 
 function processExpressions(expr, vars) {
-    var bits = [...level1ParseExpression(expr, [], [], ["[", "{", "$(", "(", "\"", "'"], "]}))\"'")];
+    var bits = [...level1ParseExpression(expr, [], [], ["[", "{", "$(", "("], "]}))", "\"'", "$(")];
     bits = bits.map(bit => {
         var m = /^\$\((.+)\)$/.exec(bit);
         if (!m) return bit;
@@ -85,7 +117,7 @@ var temp;
 const operators = [
     // Unary
     {
-        $: (left, right, vars) => [left, vars[right]],
+        $: (left, right, vars) => [left, (right in vars ? vars[right] : (() => { throw "no var " + right; })())],
     },
     {
         ["."]: (left, right) => [left[right]],
@@ -121,11 +153,11 @@ const operators = [
     // Comparison
     {
         ["<"]: (left, right) => [left < right],
-        [">"]: (left, right) => [left < right],
-        ["<="]: (left, right) => [left < right],
-        [">="]: (left, right) => [left < right],
-        ["=="]: (left, right) => [left < right],
-        ["!="]: (left, right) => [left < right],
+        [">"]: (left, right) => [left > right],
+        ["<="]: (left, right) => [left <= right],
+        [">="]: (left, right) => [left >= right],
+        ["=="]: (left, right) => [left == right],
+        ["!="]: (left, right) => [left != right],
     },
     // Boolean
     {
@@ -136,7 +168,19 @@ const operators = [
     },
     // Containment
     {
-        in: (left, right) => [Array.isArray(left) || typeof left === "string" ? left.includes(right) : (() => { throw "not a container" })()],
+        in: (left, right) => [Array.isArray(left) || typeof left === "string" ? left.includes(right) : (() => { throw left + ": not a container" })()],
+    },
+    // Ifelse 1
+    {
+        if: (left, right) => [right ? { true: left } : {}],
+    },
+    // Ifelse 2
+    {
+        else: (left, right) => ["true" in left ? left.true : right],
+    },
+    // Misc
+    {
+        ["??"]: (left, right) => [left + (0 | (Math.random() * (right - left)))],
     },
 ];
 delete temp;
